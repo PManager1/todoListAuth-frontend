9fe2818bd0d680193865491b3458f48c436fdd40
jpca999 <jpca999@gmail.com>
2017-05-23 11:55:04 -0400

    username and passwod input tags showing up and the signin and signup buttons showing up

--- app/components/App.js
+++ app/components/App.js
@@ -23,60 +23,6 @@ var App =  React.createClass ({
 });
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'stretch',
-    backgroundColor: '#F5FCFF',
-  },
-  topBar:{
-    padding:16,
-    paddingTop: 28,
-    paddingBottom: 8,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#2ecc71'
-  },
-  title:{
-    color: 'white',
-    fontSize: 20,
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  inputContainer:{
-    padding: 8,
-    paddingTop: 0,
-    backgroundColor: '#2ecc71'
-  },
-  input:{
-    height: 26,
-    padding: 4,
-    paddingLeft: 8,
-    borderRadius: 8,
-    backgroundColor: 'white'
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  todoContainer: {
-    padding: 16,
-    borderTopWidth:1,
-    borderBottomWidth:1,
-    marginBottom: -1,
-    borderColor: '#ccc',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  },
-  todoText: {
-
-  },
 });
 
 module.exports = App;
--- app/components/Login.js
+++ app/components/Login.js
@@ -3,7 +3,9 @@ import React from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  TextInput,
+  View,
+  TouchableOpacity,
 } from 'react-native';
 
 var Login = React.createClass({
@@ -12,9 +14,38 @@ var Login = React.createClass({
       <View style={styles.container}>
         <View style={styles.titleContainer}>
           <Text style={styles.title}>
-            Welcome to React Native - Login View.
+            To-Do
           </Text>
         </View>
+        <View style={styles.field}>
+            <TextInput
+              placeholder="Email"
+              style={styles.textInput}
+          />
+        </View>
+
+        <View style={styles.field}>
+            <TextInput
+              placeholder="Password"
+              style={styles.textInput}
+          />
+        </View>
+
+<View style={styles.buttonContainer}>
+    <TouchableOpacity>
+      <Text style={styles.button}> SignIn
+      </Text>
+    </TouchableOpacity>
+</View>
+
+<View style={styles.buttonContainer}>
+    <TouchableOpacity>
+      <Text style={styles.button}> SignUp
+      </Text>
+    </TouchableOpacity>
+</View>
+
+
       </View>
     );
   }
@@ -22,6 +53,16 @@ var Login = React.createClass({
 
 
 const styles = StyleSheet.create({
+  buttonContainer:{
+    padding:20,
+    flexDirection:'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  button:{
+    fontSize: 30,
+    color: 'white'
+  },
   container: {
     flex: 1,
     justifyContent: 'flex-start',
@@ -34,7 +75,18 @@ const styles = StyleSheet.create({
   },
   title:{
     color: 'white',
-    fontSize: 35,
+    fontSize: 35
+  },
+  field: {
+    borderRadius: 5,
+    padding: 5,
+    paddingLeft: 8,
+    margin: 7,
+    marginTop:0,
+    backgroundColor: 'white'
+  },
+  textInput: {
+    height: 26,
   }
 });
 
